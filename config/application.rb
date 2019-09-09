48b3aa363df2fdab215ed72d359eea9c7c364c25
om0316 <masaya.ogawa@di-v.co.jp>
2019-09-09 16:09:40 +0900

    gem とか

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,13 @@ module FreemarketSample
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    
   end
 end
